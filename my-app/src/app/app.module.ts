ed07f491a4cda8bbb9dfa5b46558f54e96cc987d
Alisher Nematov <alisher.b.nematov@gmail.com>
2023-01-04 16:44:08 +0500

    feat: add course item component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LogoComponent } from './logo/logo.component';
 import { CoursesComponent } from './courses/courses.component';
+import { CourseItemComponent } from './courses/course-item/course-item.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { CoursesComponent } from './courses/courses.component';
     HeaderComponent,
     FooterComponent,
     LogoComponent,
-    CoursesComponent
+    CoursesComponent,
+    CourseItemComponent
   ],
   imports: [
     BrowserModule
