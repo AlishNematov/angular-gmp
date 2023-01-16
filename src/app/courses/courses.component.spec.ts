1e5bcd6fa8975bad7d5f3390eb72df1abf558478
Alisher Nematov <alisher.b.nematov@gmail.com>
2023-01-16 15:39:13 +0500

    feat: cover courses listing component

--- src/app/courses/courses.component.spec.ts
+++ src/app/courses/courses.component.spec.ts
@@ -1,6 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 import { CoursesComponent } from './courses.component';
 
@@ -24,4 +25,42 @@ describe('CoursesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should log message with value if onSearchButtonClick method called', () => {
+    const consoleSpy = spyOn(console, 'log');
+    component.onSearchButtonClick('test');
+    expect(consoleSpy).toHaveBeenCalledWith('test');
+  });
+
+  it('should call onSearchButtonClick if Search button clicked', () => {
+    const onSearchButtonClickSpy = spyOn(component, 'onSearchButtonClick');
+    const loadMoreButton = fixture.debugElement.query(By.css('button.search__button'));
+    loadMoreButton.nativeElement.click();
+
+    expect(onSearchButtonClickSpy).toHaveBeenCalled();
+  });
+
+  it('should log message if onLoadMoreBtnClick method called', () => {
+    const consoleSpy = spyOn(console, 'log');
+    const clickEvent = new Event('click');
+    component.onLoadMoreBtnClick(clickEvent);
+    expect(consoleSpy).toHaveBeenCalled();
+  });
+
+  it('should prevent default actions if onLoadMoreBtnClick method called', () => {
+    const clickEvent = new Event('click');
+    const preventDefaultSpy = spyOn(clickEvent, 'preventDefault');
+    component.onLoadMoreBtnClick(clickEvent);
+    expect(preventDefaultSpy).toHaveBeenCalled();
+  });
+
+  it('should call onLoadMoreBtnClick if Load More button clicked', () => {
+    const onLoadMoreBtnClickSpy = spyOn(component, 'onLoadMoreBtnClick');
+    const loadMoreButton = fixture.debugElement.query(By.css('button.courses__more-button'));
+    loadMoreButton.nativeElement.click();
+
+    expect(onLoadMoreBtnClickSpy).toHaveBeenCalled();
+  });
+
+
 });
