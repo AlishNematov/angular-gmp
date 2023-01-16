9437ca9d611545cea2542832ffc5c048833a17d6
Alisher Nematov <alisher.b.nematov@gmail.com>
2023-01-16 17:49:57 +0500

    feat: cover course item component

--- src/app/course-item/course-item.component.spec.ts
+++ src/app/course-item/course-item.component.spec.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { CourseItemComponent } from './course-item.component';
 
@@ -28,5 +29,28 @@ describe('CourseItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call onEditButtonClick if edit button clicked', () => {
+    const onEditButtonClickSpy = spyOn(component, 'onEditButtonClick');
+    const editButton = fixture.debugElement.query(By.css('button.buttons__button_edit'));
+    editButton.nativeElement.click();
+
+    expect(onEditButtonClickSpy).toHaveBeenCalled();
+  });
+
+  it('should call onDeleteButtonClick if edit button clicked', () => {
+    const onDeleteButtonClickSpy = spyOn(component, 'onDeleteButtonClick');
+    const deleteButton = fixture.debugElement.query(By.css('button.buttons__button_delete'));
+    deleteButton.nativeElement.click();
+
+    expect(onDeleteButtonClickSpy).toHaveBeenCalled();
+  });
+
+  it('should return string of duration if method transformMinutes called', () => {
+    const inputValue = 99;
+    const expectingValue = '1h 39 min';
+    const test = component.transformMinutes(inputValue);
+    expect(test).toEqual(expectingValue);
   });
 });
