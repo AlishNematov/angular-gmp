edbc1d29a9b61ed8978140f234284224caded31b
Alisher Nematov <alisher.b.nematov@gmail.com>
2023-01-29 20:41:37 +0500

    feat: add tests to search component

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,4 +1,7 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 import { SearchComponent } from './search.component';
 
@@ -8,9 +11,11 @@ describe('SearchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      imports: [ FormsModule ],
+      declarations: [ SearchComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
@@ -19,5 +24,19 @@ describe('SearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should log message with value if search method called', () => {
+    const consoleSpy = spyOn(console, 'log');
+    component.search('test');
+    expect(consoleSpy).toHaveBeenCalledWith('test');
+  });
+
+  it('should call search method if Search button clicked', () => {
+    const onSearchButtonClickSpy = spyOn(component, 'search');
+    const loadMoreButton = fixture.debugElement.query(By.css('button.search__button'));
+    loadMoreButton.nativeElement.click();
+
+    expect(onSearchButtonClickSpy).toHaveBeenCalled();
   });
 });
